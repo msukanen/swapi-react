9165ba6b1ce6b2be74ef6d650045e3b5556c6ea4
Markku Sukanen <markku.sukanen@gmail.com>
2021-09-23 12:41:44 +0300

    urls, moving imgs

---
+++ src/LocalUrl.js
@@ -0,0 +1,3 @@
+export function urlGfx(src) {
+    return `${process.env.PUBLIC_URL}/gfx/${src}`
+}
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,32 +1,33 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import { github } from './About'
+import { urlGfx } from '../LocalUrl'
 
 class Nav extends React.Component {
-    render() {return(
-	<nav className="navbar navbar-inverse navbar-fixed-top">
+    render() {return(<nav className="navbar navbar-inverse navbar-fixed-top">
 		<div className="container">
 			<div className="navbar-header">
-				<button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+				<button type="button" className="navbar-toggle collapsed"
+						data-toggle="collapse" data-target="#navbar"
+						aria-expanded="false"  aria-controls="navbar">
 					<span className="sr-only">Toggle navigation</span>
 					<span className="icon-bar"></span>
 					<span className="icon-bar"></span>
 					<span className="icon-bar"></span>
 				</button>
 				<a className="navbar-brand" href={`${github}`} target="github-swapi-react">
-					<img src={`${process.env.PUBLIC_URL}/gfx/yoda.gif`} height="64" alt="Fetch-H#1 (SWAPI)"/>
+					<img src={urlGfx('yoda.gif')} height="64" alt="Fetch-H#1 (SWAPI)"/>
 				</a>
 			</div>
 			<div id="navbar" className="collapse navbar-collapse">
 				<ul className="nav navbar-nav">
-					<li className="active"><a onClick={() => {this.props.stateFunc(0)}}><img src="home.svg" width="22" height="22" alt="Home" /></a></li>
-					<li><a onClick={() => {this.props.stateFunc('about')}}><img src="question-mark.svg" width="22" height="22" alt="About" /></a></li>
+					<li className="active"><a onClick={() => {this.props.stateFunc(0)}}><img src={urlGfx('home.svg')} width="22" height="22" alt="Home" /></a></li>
+					<li><a onClick={() => {this.props.stateFunc('about')  }}><img src={urlGfx('question-mark.svg')} width="22" height="22" alt="About" /></a></li>
 					<li><a onClick={() => {this.props.stateFunc('contact')}}>Contact</a></li>
 				</ul>
 			</div>
 		</div>
-	</nav>
-    )}
+	</nav>)}
 }
 
 export default Nav
